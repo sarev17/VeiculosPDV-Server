5ca4862b57fb7d34825333a492367ba8fe82dda8
sarev17 <sarev17@gmail.com>
2021-12-27 13:38:17 -0300

    qrcode pdf

--- app/Http/Controllers/PagamentoController.php
+++ app/Http/Controllers/PagamentoController.php
@@ -5,59 +5,65 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pagamento;
 use App\Models\Venda;
+use Illuminate\Support\Facades\App;
 
 class PagamentoController extends Controller
 {
     public function store(Request $r)
     {
-        date_default_timezone_set ("America/Sao_Paulo");
+        date_default_timezone_set("America/Sao_Paulo");
         $p = new Pagamento;
         $v = new Venda;
 
-        $atual = explode('/',$r->pagas);
-        
-        if(intval($r->adiantar)>1){
-            $refA = intval($atual[0])+intval($r->adiantar)-1;
-        }else{
+        $atual = explode('/', $r->pagas);
+
+        if (intval($r->adiantar) > 1) {
+            $refA = intval($atual[0]) + intval($r->adiantar) - 1;
+        } else {
             $refA =  intval($atual[0]);
         }
 
-        $ref = "Parcela ".$refA;
-        
+        $ref = "Parcela " . $refA;
+
 
 
         $parc = intval($atual[0]);
 
-        $total = floatval(preg_replace('/\D/','',$r->total))/100;
+        $total = floatval(preg_replace('/\D/', '', $r->total)) / 100;
 
         $p->cliente = $r->cliente;
         $p->cpf = $r->cpf;
         $p->cliente = $r->cliente;
         $p->veiculo = $r->veiculo;
         $p->referencia = $ref;
-        $p->total =$total;
+        $p->total = $total;
         $p->venda_id = $r->idv;
         $p->updated_at = date('Y-m-d H:i:s');
         $p->created_at = date('Y-m-d H:i:s');
 
-        
+
         //atualizando pagamento na tabela vendas
-        Venda::where('id',$r->idv)->update(['pagas'=>$refA]);
-        
+        Venda::where('id', $r->idv)->update(['pagas' => $refA]);
+
         //testando se restam parcelas
-        if(intval($atual[0])==intval($atual[1])){
-            Venda::where('id',$r->idv)->update(['status'=>'fechada']);
+        if (intval($atual[0]) == intval($atual[1])) {
+            Venda::where('id', $r->idv)->update(['status' => 'fechada']);
         }
 
         $p->save();
         echo '<script>alert("Salvo")</script>';
-        return redirect()->route('principal');
-      
+
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadHTML(view('PDF.comp_pagamento', ['r' => $r]))->setPaper('A4', 'portrait');
+        return $pdf->stream('Comprovante ' . date('Y') . '.pdf', ['Attachment' => false]);
+        //return view('PDF.entradas', ['pagamentos' => $pagamentos, 'total' => $total, 'entradas' => $entradas,'hoje'=>$hoje,'responsavel'=>$responsavel]);
+
+
+
     }
 
     public function listar()
-    {
-        {
+    { {
             //return $dataTable->render('telas.dados.estoque');
             //$produtos = Veiculo::orderby('id', 'desc')->paginate();
             $produtos = Pagamento::get();
@@ -66,10 +72,9 @@ class PagamentoController extends Controller
     }
     public function date()
     {
-        $dados = explode('d',key($_REQUEST));
-        
-        $dados = Pagamento::whereBetween('created_at',array($dados[0].' 00:00:00',$dados[1].' 23:59:59'))->get();
+        $dados = explode('d', key($_REQUEST));
+
+        $dados = Pagamento::whereBetween('created_at', array($dados[0] . ' 00:00:00', $dados[1] . ' 23:59:59'))->get();
         return view('telas.dados.entradas', ['produtos' => $dados]);
     }
 }
-
--- resources/views/func/ajax.php
+++ resources/views/func/ajax.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 use App\Models\Veiculo;
 use App\Models\Venda;
 
@@ -9,12 +10,17 @@ $model = $valores[0]['valores'][0];
 $coluna = $valores[0]['valores'][1];
 $dado = $valores[0]['valores'][2];
 
-
-if($model=='Veiculo')
-    $busca = array(Veiculo::where($coluna,$dado)->get());
-if($model=='Venda')
-    $busca = array(Venda::where($coluna,$dado)->where('status','aberta')->get());
-
-echo $busca[0][0];
-
-?>
+if (isset($valores[0]['valores'][3])) {
+    
+    $busca = Venda::where($coluna,'like', '%'.$dado.'%')->get();
+        echo json_encode($busca);
+} else {
+    if ($model == 'Veiculo') {
+        $busca = array(Veiculo::where($coluna, $dado)->get());
+        echo $busca[0][0];
+    }
+    if ($model == 'Venda') {
+        $busca = array(Venda::where($coluna, $dado)->where('status', 'aberta')->get());
+        echo $busca[0][0];
+    }
+}
